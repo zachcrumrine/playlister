c5c44b82140f65fadb82eae118aebf58899ff041
zachcrumrine <zacharycrumrine@alum.calarts.edu>
2014-10-27 11:33:19 -0700

    with migration and heroku

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,4 @@
 class Song < ActiveRecord::Base
+	belongs_to :playlist
+	validates_presence_of :title
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :songs
 
   resources :playlists
